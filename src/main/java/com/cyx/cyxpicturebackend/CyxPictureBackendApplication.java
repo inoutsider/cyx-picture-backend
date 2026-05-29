9818efcf39f6afd72dbf5bc72e6e578434af70fa
chenyuxiang <1805193985@qq.com>
2026-05-29 10:00:03 +0800

    - init

---
+++ src/main/java/com/cyx/cyxpicturebackend/CyxPictureBackendApplication.java
@@ -0,0 +1,15 @@
+package com.cyx.cyxpicturebackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.cyx.cyxpicturebackend.mapper")
+public class CyxPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyxPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyx/cyxpicturebackend/CyxPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyx.cyxpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyxPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
